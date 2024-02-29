c92478bb1b78cb0a731579fff1f23077ba113f05
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-02-29 15:26:11 +0100

    font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
