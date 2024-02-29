ec72b6f1cfc07ac69473a40832a2b85d092c2e46
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-02-29 14:54:25 +0100

    hdhsjK

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
